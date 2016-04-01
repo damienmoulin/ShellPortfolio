f40b1b4bda7664a4d100aa53740bcbf363953629
damienmoulin <damien.moulin1@gmail.com>
2016-04-01 15:52:59 +0200

    [Function] Submit a command and add a new command input

--- web/Resources/js/main.js
+++ web/Resources/js/main.js
@@ -1,15 +1,71 @@
 jQuery(document).ready(function() {
 
+    var keyEnter = 13;
+    var container = $('.container');
+
+    //DEFINED DEFAULT VALUE :
+    var name = 'anonym',
+        server = 'shell',
+        directory = '~ $';
+
+
     window.onload = init;
     function init(){
         //Initialization
     }
 
+    //DEFINE COMMAND BLOC
+    function defineBlocCommand(){
+        var generatedBloc =
+            '<div class="bloc">' +
+                '<div class="command_block">' +
+                    '<div class="user">'+name+'@'+server+'</div>' +
+                    '<div class="directory">'+directory+'</div>' +
+                    '<div class="command"><input type="text"></div>' +
+                '</div>' +
+                '<div class="reponse">' +
+                '</div>' +
+            '</div>';
+
+        return generatedBloc;
+    }
+
+    function setNewInput()
+    {
+        //DEFINE A NEW BLOC COMMAND WITH NAME@SERVER DIRECTORY AND INPUT
+        var CommandLineBloc = defineBlocCommand();
+
+        container.append(CommandLineBloc);
+        $('input').focus();
+
+    }
+
+    //SUBMIT COMMAND
+    function submitCommand()
+    {
+        //GET INPUT VALUE
+        var command = $('input').val();
+        //GET PARENT OF THIS INPUT
+        var parent = $('input').parent();
+        // APPEND VALUE OF COMMAND ON TEXT
+        parent.text(command);
+
+
+        //CALL SCRIPT TO CHECK COMMAND
+        //DISPLAY REPONSE
+
+        //DISPLAY A NEW INPUT
+        setNewInput();
+    }
+
     //FOCUS ON INPUT
     $('html').click(function(){
-        console.log('click');
         $('input').focus();
     });
 
-    
+    $(document).keypress(function(e) {
+        if(e.which == keyEnter) {
+            submitCommand();
+        }
+    });
 });
