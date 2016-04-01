64980c9135f4ad962f75a4485cf7ee382e48c0ac
damienmoulin <damien.moulin1@gmail.com>
2016-04-01 12:53:22 +0200

    [Symfony] Init

---
+++ src/ShellPortfolioBundle/Controller/ShellDisplayController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: damien
+ * Date: 01/04/16
+ * Time: 12:28
+ */
+
+namespace ShellPortfolioBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * @Route("/")
+ */
+class ShellDisplayController extends Controller
+{
+    /**
+     * @Route("/", name="shell_index")
+     */
+    public function indexAction()
+    {
+        return $this->render('ShellDisplay/index.html.twig');
+    }
+}
